fb0236706a75ad7a98358fea346d33215890e53f
Dilip-kumar-96 <120187622+Dilip-kumar-96@users.noreply.github.com>
2025-03-12 13:51:18 +0530

    removed unwanted lines. Fully functional timer passing all test case senarios

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask, request, jsonify
-from datetime import datetime, date, time
+from flask import Flask, jsonify
+from datetime import datetime, date
 import psycopg2
 
 app = Flask(__name__)
@@ -18,12 +18,10 @@ def start_time():
     cursor.execute("select max(entry_no) from study_timer.study_timer_table")
     last_entry_no = cursor.fetchall()
     last_entry_no = last_entry_no[0][0]
-    print(last_entry_no)
     cursor.execute("select * from study_timer.study_timer_table where entry_no = %s", (last_entry_no,))
     record = cursor.fetchall()
-    print(type(record[0][3]))
-    if record[0][3] == None: #checks if the end_time is none. if it is then timer has already started.
-        return(jsonify({'message': 'timer has already started, please end it'}))
+    if record[0][3] == None:  # checks if the end_time is none. if it is then timer has already started.
+        return (jsonify({'message': 'timer has already started, please end it'}))
     cursor.execute(
         "insert into study_timer.study_timer_table (entry_date, start_time) values (current_date, current_time)")
 
@@ -44,7 +42,8 @@ def end_time():
     if record[0][3] != None:  # checks if the latest entry has ended or not. if it is ended it throws error.
         return (jsonify({'message': 'timer has already ended. Please start new one'}))
 
-    cursor.execute("update study_timer.study_timer_table set end_time = current_time where entry_no = %s", (last_entry_no,))
+    cursor.execute("update study_timer.study_timer_table set end_time = current_time where entry_no = %s",
+                   (last_entry_no,))
     cursor.execute("select * from study_timer.study_timer_table where entry_no = %s", (last_entry_no,))
     record = cursor.fetchall()
     record = record[0]
@@ -53,8 +52,8 @@ def end_time():
     time_spent = datetime.combine(date.min, end_time) - datetime.combine(date.min, start_time)
     time_spent = str(time_spent)
     time_spent = time_spent.split(".")[0]
-    print(time_spent)
-    cursor.execute("update study_timer.study_timer_table set time_spent = %s where entry_no = %s", (time_spent,last_entry_no))
+    cursor.execute("update study_timer.study_timer_table set time_spent = %s where entry_no = %s",
+                   (time_spent, last_entry_no))
 
     conn.commit()
     cursor.close()
